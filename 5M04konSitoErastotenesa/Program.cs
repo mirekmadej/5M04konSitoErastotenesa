032fe461dfd48f48717c9f0307e25d4481446366
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-10-02 12:19:18 +0200

    Dodaj pliki projektów.

---
+++ 5M04konSitoErastotenesa/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _5M04konSitoErastotenesa
+{
+    internal class Program
+    {
+        private const int N = 100;        
+        static void Main(string[] args)
+        {
+            bool[] A = new bool[N + 1];
+            for (int i = 2; i <= N; i++)
+                A[i] = true;
+
+            wypelnijTablice(A);
+
+            Console.WriteLine("Liczby pierwsze:");
+            for (int i = 2; i <= N; i++)
+                if (A[i])
+                    Console.Write($"{i}, ");
+
+        }
+        private static void wypelnijTablice(bool[] A)
+        {
+            for(int i = 2; i<= Math.Sqrt(N); i++)
+            {
+                if (A[i])
+                    for(int j = 2*i; j <=N; j=j+i)
+                        A[j] = false;
+            }    
+        }
+
+    }
+}
